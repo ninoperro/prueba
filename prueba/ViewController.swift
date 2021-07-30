2a9847888ba8154e9e81d52188cad0b3556f24e4
Alejandro <alejandro.bello@genesys.cl>
2021-07-30 16:15:08 -0400

    Initial Commit

---
+++ prueba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by GENESYS-MAC-02 on 30-07-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
